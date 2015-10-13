504f585abc1fd2e36525d979ef2f2dba2cc27e65
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-10-13 19:01:02 -0400

    added basic routing

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,35 @@
 'use strict';
 
 var React = require('react-native');
+var Main = require('./App/Components/Main');
+
 var {
   AppRegistry,
   StyleSheet,
   Text,
+  NavigatorIOS,
   View,
 } = React;
+
+var styles = StyleSheet.create({
+    container : {
+      flex: 1,
+      backgroundColor: '#11111'
+    }
+});
 
 class githubNotetaker extends React.Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Github Notetaker!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: 'Github Notetaker',
+          component: Main
+        }}
+      />
     );
   }
 };
-
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
